22dbe3dfcbca0fbf702e02872ab28fe1b68ddae6
SlawomirTwarog <slawomir.twarog@emergn.com>
2025-11-16 17:01:29 +0100

    update config

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://swapi.dev',
+      crossorigin: 'anonymous'
+    },
+    {
+      rel: 'dns-prefetch',
+      href: 'https://swapi.dev'
+    }
+  ]
+})
+</script>
